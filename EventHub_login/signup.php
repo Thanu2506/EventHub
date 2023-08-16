86ac796dd7c3c12d2739c1de3e2e0e1426a49a21
Rayappu Thanusiyan <111243866+Thanu2506@users.noreply.github.com>
2023-08-16 15:45:21 +0530

    Add files via upload

---
+++ EventHub_login/signup.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once './classes/user.php';
+require_once './classes/Dbconnector.php';
+
+use classes\user;
+use classes\Dbconnector;
+
+if (isset($_POST["Fullname"], $_POST["username"], $_POST["email"], $_POST["password"], $_POST["NIC"], $_POST["Phone"])) {
+    if (empty($_POST["Fullname"]) || empty($_POST["username"]) || empty($_POST["email"]) || empty($_POST["password"]) || empty($_POST["NIC"]) || empty($_POST["Phone"])) {
+        $location = "registation.php?status=1";
+    } else {
+        $fullname = $_POST["Fullname"];
+        $email = $_POST["email"];
+        $username = $_POST["username"];
+        $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+        $nic = $_POST["NIC"];
+        $phonenumber = $_POST["Phone"];
+        $role = "customer";
+        $user = new user($fullname, $email, $username, $password, $nic, $phonenumber, $role);
+        if($user->register(Dbconnector::getConnection())){
+            $location = "registation.php?status=2";
+        } else {
+            $location = "registation.php?status=3";
+            
+        }
+    }
+} else {
+    $location = "registation.php?status=0";
+}
+header("Location:" . $location);
